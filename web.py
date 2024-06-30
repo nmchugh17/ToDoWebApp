33be98dd09d9d0331ae7b3b9cfb5feea6f362315
nmchugh17 <nmchu17@gmail.com>
2024-06-30 11:52:14 +0100

    Removed Procfile and setup shell script

--- web.py
+++ web.py
@@ -28,5 +28,4 @@ for index, todo in enumerate(todos):
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
 
-# print("hello")
-# st.session_state
+
